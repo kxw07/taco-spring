e7963b7432229083c5c8ce78a3417f7bdad1bd14
kai <kai@simbo.com.tw>
2021-07-14 10:35:00 +0800

    init project

---
+++ src/main/java/com/example/tacospring/TacoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.tacospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tacospring/TacoSpringApplicationTests.java
@@ -0,0 +1,8 @@
+package com.example.tacospring;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoSpringApplicationTests {
+
+}
